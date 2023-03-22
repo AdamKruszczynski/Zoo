938a6d26edb83d6f296f47343e5e563e3895b0bb
AdamKruszczynski <ag27d3a6@gmail.com>
2023-03-22 20:44:29 +0100

    Stworzenie projektu. Dodanie 3 klas.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Dog dog = new Dog("Max", 4);
+        Cat cat = new Cat("Kicia", 6);
+        Turtle turtle = new Turtle("Tom", 35);
+        System.out.println(dog.name);
+        System.out.println(dog.age);
+        System.out.println(cat.name);
+        System.out.println(cat.age);
+        System.out.println(turtle.name);
+        System.out.println(turtle.age);
+
+
+    }
+}
---
+++ src/Turtle.java
@@ -0,0 +1,10 @@
+public class Turtle {
+    String name;
+    int age;
+
+
+    public Turtle(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
