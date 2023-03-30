230b1e9e05d8ebe37598aa15cf8cea9b36cd91ae
AdamKruszczynski <ag27d3a6@gmail.com>
2023-03-30 16:13:39 +0200

    Zoo-5 Zakończenie projektu

--- src/Dog.java
+++ src/Dog.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Dog extends Animal implements Eat{
     String name;
     int age;
@@ -9,5 +11,18 @@ public class Dog extends Animal implements Eat{
     @Override
     public void eat() {
         System.out.println("I eat");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Dog dog = new Dog("Max", 4);
+        Dog dog2 = new Dog("Max", 4);
         Cat cat = new Cat("Kicia", 6);
         Pig pig = new Pig("pigg", 7);
 
@@ -10,5 +11,6 @@ public class Main {
         cat.eat();
         pig.eat();
 
+        System.out.println(dog.equals(dog2));
     }
 }
